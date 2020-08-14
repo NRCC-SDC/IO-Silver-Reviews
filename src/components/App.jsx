f9104a2986d8a0f284b24ffccfcbc47d1203e0b6
Michael Brecht <brecht.michael.b@gmail.com>
2020-08-14 10:09:50 -0600

    Refactored to functional component using hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Ratings from './Ratings.jsx';
 import Reviews from './Reviews.jsx';
 import AddReview from './AddReview.jsx';
 import { Grid } from '@material-ui/core';
 
-class App extends React.Component {
-  constructor() {
-    super();
+const App = () => {
+  const [productId, setProductId] = useState(24);
+  const [meta, setMeta] = useState({});
+  const [reviews, setReviews] = useState({});
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const reviewsURL = 'http://52.26.193.201:3000/reviews/';
 
-    this.state = {
-      product_id: 24,//Math.floor(Math.random() * 100), //21
-      meta: {},
-      reviews: {},
-      modalIsOpen: false
-    }
-  }
-
-  fetchMetadata() {
-    fetch(`http://52.26.193.201:3000/reviews/${this.state.product_id}/meta`)
+  const fetchMetadata = () => {
+    const url = reviewsURL + `${productId}/meta`;
+    fetch(url)
       .then(res => res.json())
-      .then(data => {
-        this.setState({
-          meta: data
-        });
-      });
+      .then(data => setMeta(data));
   }
 
-  fetchReviews() {
-    fetch(`http://52.26.193.201:3000/reviews/${this.state.product_id}/list`)
+  const fetchReviews = () => {
+    const url = reviewsURL + `${productId}/list`;
+    fetch(url)
       .then(res => res.json())
-      .then(data => {
-        this.setState({
-          reviews: data
-        });
-      });
+      .then(data => setReviews(data));
   }
 
-  componentDidMount() {
-    this.fetchMetadata();
-    this.fetchReviews();
-  }
+  const openModal = () => setModalIsOpen(true);
 
-  addReview() {
-    this.setState({
-      modalIsOpen: true
-    });
-  }
+  const closeModal = () => setModalIsOpen(false);
 
-  closeModal() {
-    console.log('Closing modal')
-    this.setState({
-      modalIsOpen: false
-    });
-  }
+  fetchMetadata();
+  fetchReviews();
 
-  render() { 
-    return (
-      <div id="app">
-        <h2 id="title">Ratings and Reviews</h2>
+  return (
+    <div id="app">
+      <h2 id="title">
         <Grid container spacing={2}>
-          <Ratings meta={this.state.meta} />
-          <Reviews reviews={this.state.reviews} update={this.fetchReviews.bind(this)} addReview={this.addReview.bind(this)} />
+          <Ratings meta={meta} />
+          <Reviews reviews={reviews} update={fetchReviews} addReview={openModal} />
         </Grid>
-        <AddReview isOpen={this.state.modalIsOpen} closeModal={this.closeModal.bind(this)} meta={this.state.meta} update={this.fetchReviews.bind(this)} />
-      </div>
-    )
-  }
-}
+        <AddReview isOpen={modalIsOpen} closeModal={closeModal} meta={meta} update={fetchReviews} />
+      </h2>
+    </div>
+  );
+};
 
 export default App;
