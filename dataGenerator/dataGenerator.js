45e6ccf3fa3e40917904ca862e4d37a8127f7ceb
Noah Rosenthal <noahrosenthal@comcast.net>
2020-08-28 20:01:20 -0600

    Change dataGenerator.js to generate 4 csv files

--- dataGenerator/dataGenerator.js
+++ dataGenerator/dataGenerator.js
@@ -1,55 +1,112 @@
 const faker = require('faker');
 const fs = require('fs');
+const moment = require('moment');
 
-var writer = fs.createWriteStream('dataGenerator/generatedReviews.txt', {
-  flags: 'a' // 'a' means appending (old data will be preserved)
+// var writer = fs.createWriteStream('dataGenerator/generatedReviews.txt', {
+//   flags: 'a' // 'a' means appending (old data will be preserved)
+// })
+
+const reviewWriter = fs.createWriteStream('dataGenerator/generatedReviews.csv')
+
+const imageWriter = fs.createWriteStream('dataGenerator/generatedImages.csv')
+
+const revCharWriter = fs.createWriteStream('dataGenerator/generatedRevsChars.csv')
+
+const charWriter = fs.createWriteStream('dataGenerator/generatedChars.csv');
+
+reviewWriter.write('id,product_id,summary,body,rating,name,email,date,recommend,helpfulness,response,reported\n');
+
+imageWriter.write('id,url,review_id\n')
+
+charWriter.write('id,char_name\n')
+
+let charas = ['Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit'];
+charas.forEach((char, index) => {
+  charWriter.write((index + 1) + ',' + char + '\n');
 })
+charWriter.end();
+
+revCharWriter.write('id,review_id,char_id,value\n');
+
+let imageCount = 1;
+let revCharCount = 1;
 
 // let numOfReviews = 10000000;
-let numOfReviews = 1;
+let numOfReviews = 10;
 
-for (let i = 0; i < numOfReviews; i++) {
+for (let i = 1; i <= numOfReviews; i++) {
+  let dateFormatted = moment(faker.date.past()).format("YYYY[-]MM[-]DD[T]HH:mm:ss.SSS[Z]");
+
   let reviewData = {
-    id: i,
+    id: i, // added automatically with serial
     product_id: random(0, 1000000),
     summary: faker.lorem.words(),
     body: faker.lorem.paragraph(),
     rating: random(1, 5),
     name: faker.name.findName(),
     email: faker.internet.email(),
-    date: faker.date.past(),
-    recommend: random(0, 1),
+    date: dateFormatted,
+    recommend: Boolean(random(0, 1)),
+    helpfulness: random(0, 100),
     // should have response for 1 in 10 reviews
     response: random(1, 10) === 5 ? faker.lorem.sentences() : null,
     // should randomly make 1 in 1000 reviews reported
-    reported: random(1, 1000) === 500,
-    characteristics: {},
-    images: []
+    reported: random(1, 1000) === 500
   };
 
-  let charas = ['Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit'];
+  let revDataArray = [];
+  for (let key in reviewData) {
+    revDataArray.push(reviewData[key]);
+  }
+
+  for (let j = 0; j < revDataArray.length; j++) {
+    let revData;
+    if (revDataArray[j] === null) {
+      revData = revDataArray[j];
+    } else {
+      revData = revDataArray[j].toString();
+    }
+
+    if (j !== revDataArray.length - 1) {
+      revData += ','
+    }
+
+    reviewWriter.write(revData);
+  }
+
+  reviewWriter.write('\n');
+
+
+  let characteristics = {};
   let numOfChars = random(1, 6);
+  let available = [1, 2, 3, 4, 5, 6]
   // generate characteristics
   while (numOfChars > 0) {
-    let randomIndex = random(0, charas.length - 1)
-    let chara = charas[randomIndex];
-    reviewData.characteristics[chara] = random(1, 5);
-    charas = charas.slice(0, randomIndex).concat(charas.slice(randomIndex + 1));
+    let randomIndex = random(0, available.length - 1)
+    let chara = charas[available[randomIndex]];
+    revCharWriter.write(revCharCount + ',' + i + ',' + available[randomIndex] + ',' + random(1, 5) + '\n');
+    revCharCount++;
+    available = available.slice(0, randomIndex).concat(available.slice(randomIndex + 1));
     numOfChars--;
   }
 
   // generate images
   let numOfImages = random(0, 5);
   for (let j = 0; j < numOfImages; j++) {
-    reviewData.images.push(faker.random.image());
+    imageWriter.write(imageCount + ',' + faker.random.image() + ',' + i + '\n');
+    imageCount++;
   }
 
-  // console.log(reviewData);
-  const reviewDataJSON = JSON.stringify(reviewData);
-  // load reviewData into file
-  writer.write(reviewDataJSON + '\n');
+
+  // // console.log(reviewData);
+  // const reviewDataJSON = JSON.stringify(reviewData);
+  // // load reviewData into file
+  // writer.write(reviewDataJSON + '\n');
 }
-writer.end();
+reviewWriter.end();
+revCharWriter.end();
+imageWriter.end();
+
 
 // helper functions
 function random(start, end) {
--- dataGenerator/databaseLoader.js
+++ dataGenerator/databaseLoader.js
@@ -1,14 +1,57 @@
-
+const pg = require('pg');
 const lineReader = require('line-reader');
 
-let reviews = [];
+const connectionString = "postgres://noahr@localhost/bsh-rrdb";
+const pgClient = new pg.Client(connectionString);
+pgClient.connect();
+
 
 lineReader.eachLine('dataGenerator/generatedReviews.txt', (line, last) => {
-  // instead of pushing, I will be loading into database
-  reviews.push(JSON.parse(line));
+  let reviewObj = JSON.parse(line);
 
+  let insertReview = 'INSERT INTO reviews ( product_id, summary, body, response, rating, name, email, date, recommend, reported, helpfulness ) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 ) RETURNING id';
+
+  let reviewValues = [
+    reviewObj.product_id,
+    reviewObj.summary,
+    reviewObj.body,
+    reviewObj.response,
+    reviewObj.rating,
+    reviewObj.name,
+    reviewObj.email,
+    reviewObj.date,
+    Boolean(reviewObj.recommend),
+    reviewObj.reported,
+    reviewObj.helpfulness
+  ];
+
+  pgClient.query(insertReview, reviewValues)
+    .then((res) => {
+      let review_id = res.rows[0].id;
+      let insertImage = 'INSERT INTO images ( url, review_id ) VALUES ( $1, $2 )';
+
+      for (let i = 0; i < reviewObj.images.length; i++) {
+        let imageValues = [reviewObj.images[i], review_id];
+
+        pgClient.query(insertImage, imageValues);
+      }
+
+      let insertChar = 'INSERT INTO chars ( char_name ) VALUES ( $1 ) ON CONFLICT ( char_name ) DO UPDATE SET char_name=EXCLUDED.char_name RETURNING id';
+
+      for (let char in reviewObj.characteristics) {
+        pgClient.query(insertChar, [char])
+          .then((res) => {
+            let char_id = res.rows[0].id;
+
+            let insertCharReview = 'INSERT INTO reviews_chars ( review_id, char_id, value ) VALUES ( $1, $2, $3 )';
+
+            pgClient.query(insertCharReview, [review_id, char_id, reviewObj.characteristics[char]])
+          })
+      }
+    })
+
+  console.log(last);
   if (last) {
-    console.log('DataBase Loading Complete!');
+    console.log('Last Data Loaded');
   }
 });
-
